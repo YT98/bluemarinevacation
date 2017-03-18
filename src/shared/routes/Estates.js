9b07cc0bf0d29c850bf92742bbce3b657385f218
Yannis Tarfa <yannistarfa98@gmail.com>
2017-03-18 17:46:30 -0400

    Changed estates page to have map span half of width. Started working on saint-martin page.

--- src/shared/routes/Estates.js
+++ src/shared/routes/Estates.js
@@ -16,19 +16,32 @@ export default class Estates extends React.Component {
             lat: 1,
             lng: 1
         }
+      },
+      {
+        center: {
+          lat: 1.01,
+          lng: 1.01
+        }
       }
     ]
 
     return(
       <div>
+
         <div class="estates-header">
           <Navbar />
           <h1> Estates </h1>
         </div>
 
-        <div class="map-container">
-          <EstatesMap center={center} markers={markers} />
+        <div class="estates-container">
+          <div class="pane-container">
+            <p> Estates here. </p>
+          </div>
+          <div class="map-container">
+            <EstatesMap center={center} markers={markers} />
+          </div>
         </div>
+
       </div>
     );
   };
--- src/shared/routes/SaintMartin.js
+++ src/shared/routes/SaintMartin.js
@@ -9,6 +9,43 @@ export default class SaintMartin extends React.Component {
           <Navbar />
           <h1> Saint-Martin </h1>
         </div>
+
+        <div style={{marginTop: '50px'}} class="row">
+          <div class="col-md-6">
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          </div>
+          <div class="col-md-6">
+          </div>
+        </div>
+
+        <div style={{marginTop: '50px'}} class="row">
+          <div class="col-md-6">
+            <h2> Geographie </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          </div>
+          <div class="col-md-6">
+          </div>
+        </div>
+
+        <div style={{marginTop: '50px'}} class="row">
+          <div class="col-md-6">
+            <h2> Histoire </h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          </div>
+          <div class="col-md-6">
+            <h2> Monuments </h2>
+            <ul>
+              <li> 1 </li>
+              <li> 2 </li>
+              <li> 3 </li>
+              <li> 4 </li>
+              <li> 5 </li>
+              <li> 6 </li>
+              <li> 7 </li>
+            </ul>
+          </div>
+        </div>
+
       </div>
     );
   };
