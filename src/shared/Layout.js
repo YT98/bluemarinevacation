4ed3a7109a57e7158d11abd8f272d1930776c2ba
Yannis Tarfa <yannistarfa98@gmail.com>
2017-04-03 18:51:46 -0400

    Added Footer, Still have to style it properly.

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/Footer';
 
 export default class Layout extends React.Component {
   render() {
     return(
       <div>
         {this.props.children}
+        <Footer />
       </div>
     );
   };
---
+++ src/shared/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+    <div class="footer">
+      <div class="footer-navigation">
+        <h4> Accueil </h4>
+        <h4> Saint-Martin </h4>
+        <h4> Nos Propriétés </h4>
+        <h4> Contact </h4>
+      </div>
+      <div>
+        <p> Copyright </p>
+        <p> Designed by Yannis Tarfa </p>
+      </div>
+    </div>
+    );
+  };
+};
