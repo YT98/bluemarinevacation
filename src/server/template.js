e7e3fafd2aed556ca957659d8bb06bbf198a70c6
Yannis Tarfa <yannistarfa98@gmail.com>
2017-05-23 22:15:45 -0400

    Added google analytics.

--- src/server/template.js
+++ src/server/template.js
@@ -16,6 +16,15 @@ export default ({ body, lang }) => {
         <div id="root">${body}</div>
       </body>
 
+      <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-99830702-1', 'auto');
+      ga('send', 'pageview');
+      </script>
+      
       <script src="/dist/client/client.bundle.js"></script>
       <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
